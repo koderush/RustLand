a27a81a884e544fddc9de7bb392bac41412bbf91
koderush <koderush@outlook.com>
2021-04-24 23:50:46 -0600

    Complete the Guess game.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
-mod feature1;
 mod database;
+mod feature1;
 
 fn main() {
-    feature1::calculate_fab(35000000);
+    // feature1::calculate_fab(35000000);
 
-    let records = database::get_edrsystem_records();
+    // let records = database::get_edrsystem_records();
 
-    println!("Found {} edr.edrsystem records", records.unwrap().len());
+    // println!("Found {} edr.edrsystem records", records.unwrap().len());
 
-    println!("Please input your guess.");
+    let mut rng = rand::thread_rng();
 
-    let mut guess = String::new();
+    let secret_number: u32 = rng.gen_range(0..10);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}({})", guess.trim(), guess.trim().len());
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Wrong format, try again.");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
